62e84e6d1aff5ea9f229df1484a0ede3bc911298
Tyler Compton <xaviosx@gmail.com>
2023-11-28 13:04:00 -0800

    Redesign events

--- eink_calendar/ui/event.py
+++ eink_calendar/ui/event.py
@@ -1,5 +1,3 @@
-import ctypes
-
 import sdl2
 from sdl2 import sdlttf
 
@@ -9,11 +7,11 @@ from .. import api, ui
 class Event:
     HEIGHT = 40
     PADDING_BOTTOM = 10
-    WIDTH = 470
-    THICKNESS = 4
-
     START_X = 65
     START_Y = 65
+    COLOR_SQUARE_SIZE = 15
+    TEXT_X_OFFSET = 30
+    TIME_Y_OFFSET = 18
 
     def __init__(
         self,
@@ -28,32 +26,42 @@ class Event:
             r=api_event.color[0], g=api_event.color[1], b=api_event.color[2], a=255
         )
 
-        self.rect = sdl2.SDL_Rect(x=x, y=y, w=self.WIDTH, h=self.HEIGHT)
-        self.inner_rect = sdl2.SDL_Rect(
-            x=x + self.THICKNESS,
-            y=y + self.THICKNESS,
-            w=self.WIDTH - self.THICKNESS * 2,
-            h=self.HEIGHT - self.THICKNESS * 2,
+        self.color_square = sdl2.SDL_Rect(
+            x=x,
+            y=int(y + (self.HEIGHT / 2 - self.COLOR_SQUARE_SIZE / 2)),
+            w=self.COLOR_SQUARE_SIZE,
+            h=self.COLOR_SQUARE_SIZE,
         )
 
-        text = ""
-        if not api_event.all_day:
+        if api_event.all_day:
+            time_str = "All Day"
+        else:
             time_str = api_event.start_time.strftime("%I:%M %p")
-            text += f"{time_str} - "
-        text += f"{api_event.summary}"
 
-        self.ui_text = ui.Text(
-            renderer, font, text, sdl2.SDL_Color(0, 0, 0, 255), x=x, y=y, max_chars=60
+        self.time_text = ui.Text(
+            renderer,
+            font,
+            time_str,
+            sdl2.SDL_Color(0, 0, 0, 255),
+            x=x + self.TEXT_X_OFFSET,
+            y=y,
+            max_chars=60,
         )
-        self.ui_text.set_position(x + 10, int(y + (self.ui_text.rect.h / 2)))
+        self.ui_text = ui.Text(
+            renderer,
+            font,
+            api_event.summary,
+            sdl2.SDL_Color(0, 0, 0, 255),
+            x=x + self.TEXT_X_OFFSET,
+            y=y + self.TIME_Y_OFFSET,
+            max_chars=60,
+        )
 
     def draw(self, renderer: sdl2.SDL_Renderer) -> None:
         sdl2.SDL_SetRenderDrawColor(
             renderer, self.color.r, self.color.g, self.color.b, self.color.a
         )
-        sdl2.SDL_RenderFillRect(renderer, self.rect)
+        sdl2.SDL_RenderFillRect(renderer, self.color_square)
 
-        sdl2.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255)
-        sdl2.SDL_RenderFillRect(renderer, self.inner_rect)
-
+        self.time_text.draw(renderer)
         self.ui_text.draw(renderer)
